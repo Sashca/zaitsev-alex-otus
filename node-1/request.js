876a430868b96c9d3d17b7d28a262940b058b7d2
Sasha <sashca@xakep.ru>
2018-11-11 00:18:09 +0500

    homework 3

---
+++ node-1/request.js
@@ -0,0 +1,35 @@
+let fetch = require('node-fetch');
+var argv = require("optimist").argv;
+var http = require("http");
+var n    = 1;
+var type = "p";
+
+if (argv.n !== undefined) {
+    if (argv.n > 0) n = argv.n;
+}
+
+if (argv.t !== undefined) {
+    if (argv.t === "p" || argv.t === "s") type = argv.t;
+}
+
+var requests = new Array(n);
+requests.fill("http://localhost:3000");
+
+if (type == "p") {
+    Promise.all(requests.map(fetch)).then(responses => {
+        console.log("finish requests");
+    });
+} else {
+    requests.reduce(function (item, value) {
+        return item.then(() => {
+            return fetch(value).then(response => {
+                console.log("finish request");
+            });
+        })
+    }, Promise.resolve());
+}
+
+
+
+
+
